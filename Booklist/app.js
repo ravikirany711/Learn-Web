87d13783769526208b9750eb30d1092ef46eb1b3
ravikirany711 <ravikirany711@gmail.com>
2019-02-15 19:38:12 +0530

    booklist traversy

---
+++ Booklist/app.js
@@ -0,0 +1,25 @@
+//Book class: It represents a book
+class Book{
+    constructor(title,author,isbn){
+        this.title=title;
+        this.author=author;
+        this.isbn=isbn;
+
+    }
+}
+
+//UI class: Handle UI tasks
+class UI{
+    static displayBooks(){
+        const 
+
+    }
+}
+
+//store class: Handles storage
+
+//Event: To display a book
+
+//Event: To add a book
+
+//Event: To remove a book
