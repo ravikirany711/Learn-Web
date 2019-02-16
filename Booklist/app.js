0103cd00fbdb80a38a4d8ef1373575a1c1886ba3
ravikirany711 <ravikirany711@gmail.com>
2019-02-16 12:18:13 +0530

    little improvement

--- Booklist/app.js
+++ Booklist/app.js
@@ -11,14 +11,45 @@ class Book{
 //UI class: Handle UI tasks
 class UI{
     static displayBooks(){
-        const 
+        const storedBooks=[
+            {
+            title:"book1",
+            author:"danley",
+            isbn:"1234"
+            },
+            {
+                title:"book2",
+                author:"danloop",
+                isbn:"4567"
+                }
+        ];
+        const books=storedBooks
+        books.forEach((book)=>{
+            UI.addBookToList(book)
+
+        })
 
     }
+   static addBookToList(book){
+       const list=document.querySelector('#booklist')
+       const row=document.createElement('tr')
+
+        row.innerHTML=`
+        <td> ${book.title}</td>
+        <td> ${book.author}</td>
+        <td> ${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        
+
+        `;
+        list.appendChild(row)
+   }
 }
 
 //store class: Handles storage
 
 //Event: To display a book
+document.addEventListener("DOMContentLoades",UI.displayBooks)
 
 //Event: To add a book
 
